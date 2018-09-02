976b936239ad506fd2ab4a4999144f1c2f905680
sjorgensen <shanejorgensensd@gmail.com>
2018-09-02 14:34:17 -0600

    JSON File - Completed
    JSON File - Completed

--- Colton_Jorgensen_NewsBasic/js/blog.js
+++ Colton_Jorgensen_NewsBasic/js/blog.js
@@ -1,1 +1,25 @@
 document.querySelector("#blog h2").innerHTML = "New Text";
+
+var musicPage = new XMLHttpRequest();
+musicPage.onload = function(){
+	
+	data = JSON.parse(musicPage.responseText);
+	
+	var placementPlace = document.querySelector('#media');
+	if(placementPlace){
+		var placements '';
+		for (var i= 0; i<data.placements.length;i++){
+			placements+='<article>';
+			placements+='</article>';
+			
+			
+		}
+		 placementPlace.querySelector('h2').insertAdjacentHTML('afterend', placements);
+	
+	
+}
+
+
+musicPage.open('GET', 'https://ColtonJorgensen_ProjectAndPortfolioIII/Colton_Jorgensen_NewsBasic/mixDatabase.json',true);
+    musicPage.sent(null);
+
