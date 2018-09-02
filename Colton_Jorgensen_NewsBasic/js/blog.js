a781ba64b72afb3808360d32793c21698dda07d3
sjorgensen <shanejorgensensd@gmail.com>
2018-09-02 14:51:56 -0600

    Updated JSON
    Updated JSON

--- Colton_Jorgensen_NewsBasic/js/blog.js
+++ Colton_Jorgensen_NewsBasic/js/blog.js
@@ -1,16 +1,18 @@
 document.querySelector("#blog h2").innerHTML = "New Text";
 
+
 var musicPage = new XMLHttpRequest();
 musicPage.onload = function(){
 	
-	data = JSON.parse(musicPage.responseText);
+	var mixes = JSON.parse(musicPage.responseText);
 	
-	var placementPlace = document.querySelector('#media');
+	var placementPlace = document.querySelector('#mixes');
 	if(placementPlace){
-		var placements '';
-		for (var i= 0; i<data.placements.length;i++){
-			placements+='<article>';
-			placements+='</article>';
+		var mixes '';
+		for (var i= 0; i<data.mixes.length;i++){
+			mixes+='<article>';
+			mixes+='<p class="thumbnail"><img src="' + data.placements[i].imageFile+'"alt"' +;data.placements[i].placement+'"></p>';
+			mixes+='</article>';
 			
 			
 		}
