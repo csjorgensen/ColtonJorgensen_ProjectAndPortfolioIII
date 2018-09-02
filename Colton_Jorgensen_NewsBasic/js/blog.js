c5fc00b34f8f0da6ccd68fae629a306dda247256
sjorgensen <shanejorgensensd@gmail.com>
2018-09-02 15:25:44 -0600

    Updated Javascript File
    Updated Javascript File

--- Colton_Jorgensen_NewsBasic/js/blog.js
+++ Colton_Jorgensen_NewsBasic/js/blog.js
@@ -4,24 +4,24 @@ document.querySelector("#blog h2").innerHTML = "New Text";
 var musicPage = new XMLHttpRequest();
 musicPage.onload = function(){
 	
-	var mixes = JSON.parse(musicPage.responseText);
+	var data  = JSON.parse(musicPage.responseText);
 	
 	var placementPlace = document.querySelector('#mixes');
 	if(placementPlace){
-		var mixes '';
+		var mixes = '';
 		for (var i= 0; i<data.mixes.length;i++){
-			mixes+='<article>';
-			mixes+='<p class="thumbnail"><img src="' + data.placements[i].imageFile+'"alt"' +;data.placements[i].placement+'"></p>';
-			mixes+='</article>';
+			mixes += '<article>';
+			mixes += '<p class="thumbnail"><img src="' + data.mixes[i].imageFile+'"alt"' +data.mixes[i].media+'"></p>';
+			mixes += '</article>';
 			
 			
 		}
-		 placementPlace.querySelector('h2').insertAdjacentHTML('afterend', placements);
-	
+		 placementPlace.querySelector('h2').insertAdjacentHTML('afterend', mixes);
+	}
 	
 }
 
 
 musicPage.open('GET', 'https://ColtonJorgensen_ProjectAndPortfolioIII/Colton_Jorgensen_NewsBasic/mixDatabase.json',true);
-    musicPage.sent(null);
+musicPage.send(null);
 
