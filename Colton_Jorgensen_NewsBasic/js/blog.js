5b87d77f27ce25544e426011984fe4889b3a49db
sjorgensen <shanejorgensensd@gmail.com>
2018-09-04 22:34:44 -0600

    Final JSON update
    Final JSON update

--- Colton_Jorgensen_NewsBasic/js/blog.js
+++ Colton_Jorgensen_NewsBasic/js/blog.js
@@ -1,20 +1,21 @@
 document.querySelector("#blog h2").innerHTML = "DJ Colton Jorgensen";
 
 
-var musicPage = new XMLHttpRequest();
-musicPage.onload = function(){
+var xml = new XMLHttpRequest();
+xml.onload = function(){
 	
-	var data  = JSON.parse(musicPage.responseText);
+var data = JSON.parse(xml.responseText);
 	
 	var placementPlace = document.querySelector('#blog');
 	if(placementPlace){
 		var mixes = '';
 		for (var i= 0; i<data.mixes.length;i++){
 			mixes += '<article>';
-			mixes += '<p class="thumbnail"><img src="' + data.mixes[i].imageFile+'"alt"' +data.mixes[i].media+'"></p>';
-			mixes += '<h3 class="h2">'+data.mixes[i].mixName+'"></h3>';
+			mixes += '<p class="thumbnail"><img src="' + data.mixes[i].imageFile+'"alt"' +data.mixes[i].imageFile+'"></p>';
+			mixes += '<h3 class="h3">'+data.mixes[i].mixName+'"></h3>';
 			mixes += '</article>';
 			console.log(data.mixes[1].mixName);
+			
 			
 			
 		}
@@ -24,6 +25,6 @@ musicPage.onload = function(){
 }
 
 
-musicPage.open('GET', 'https://raw.githubusercontent.com/csjorgensen/ColtonJorgensen_ProjectAndPortfolioIII/master/Colton_Jorgensen_NewsBasic/mixDatabase.json',true);
-musicPage.send("null");
+xml.open('GET', 'https://csjorgensen.github.io/ColtonJorgensen_ProjectAndPortfolioIII/Colton_Jorgensen_NewsBasic/mixDatabase.json',true);
+xml.send(null);
 
