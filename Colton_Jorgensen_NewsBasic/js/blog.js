43d63aa10a058d44b2432cf60ac85226da78e422
sjorgensen <shanejorgensensd@gmail.com>
2018-09-07 06:19:51 -0600

    Gallery JSON
    Gallery JSON

--- Colton_Jorgensen_NewsBasic/js/blog.js
+++ Colton_Jorgensen_NewsBasic/js/blog.js
@@ -13,24 +13,22 @@ var data = JSON.parse(xml.responseText);
 			mixes += '<article>';
 			mixes += '<p class="thumbnail"><img src="' + data.mixes[i].imageFile+'"alt"' +data.mixes[i].imageFile+'"></p>';
 			mixes += '<h3 class="h3">'+data.mixes[i].mixName+'</h3>';
+			mixes += '<dl>';
 			mixes += '<dt>Date Recorded: </dt>';
 			mixes += '<dd>'+data.mixes[i].DateRecorded+'</dd>';
 			mixes += '<dt>Genre: </dt>';
 			mixes += '<dd>'+data.mixes[i].Genre+'</dd>';
+			mixes += '<dt>BPM: </dt>';
+			mixes += '<dd>'+data.mixes[i].BPM+'</dd>';
 			mixes += '<dt>Description </dt>';
 			mixes += '<dd>'+data.mixes[i].description+'</dd>';
-			mixes += '</article>';
-		
-			
-			
-			
+			mixes += '</dl>';
+			mixes += '<p><a href="#" link="">Read More</a></p>'
+			mixes += '</article>';	
 		}
 		 placementPlace.querySelector('h2').insertAdjacentHTML('afterend', mixes);
 	}
-	
 }
-
-
 xml.open('GET', 'https://csjorgensen.github.io/ColtonJorgensen_ProjectAndPortfolioIII/Colton_Jorgensen_NewsBasic/mixDatabase.json',true);
 xml.send(null);
 
