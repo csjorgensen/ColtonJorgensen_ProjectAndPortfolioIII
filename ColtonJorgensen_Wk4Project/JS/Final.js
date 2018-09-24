d6a53b143b7955a8b8566604d73fee31dfe61cea
sjorgensen <shanejorgensensd@gmail.com>
2018-09-23 21:38:45 -0600

    Update to Final Output
    Update to Final Output

--- ColtonJorgensen_Wk4Project/JS/Final.js
+++ ColtonJorgensen_Wk4Project/JS/Final.js
@@ -1,28 +1,29 @@
- function createNode(element) {
-      return document.createElement(element);
-  }
+function createNode(element) {
+  return document.createElement(element);
+}
 
-  function append(parent, el) {
-    return parent.appendChild(el);
-  }
+function append(parent, el) {
+  return parent.appendChild(el);
+}
 
-  const ul = document.getElementById('authors');
-  const url = 'https://randomuser.me/api/?results=10';
-  fetch(url)
-  .then((resp) => resp.json())
-  .then(function(data) {
-    let authors = data.results;
-    return authors.map(function(author) {
-      let li = createNode('li'),
-          img = createNode('img'),
-          span = createNode('span');
-      img.src = author.picture.medium;
-      span.innerHTML = `${author.name.first} ${author.name.last}`;
-      append(li, img);
-      append(li, span);
-      append(ul, li);
-    })
-  })
-  .catch(function(error) {
-    console.log(error);
-  });   
+var ul = document.getElementById('djs');
+var url = 'https://randomuser.me/api/?results=6';
+fetch(url).then(function (resp) {
+  return resp.json();
+}).then(function (data) {
+  var djs = data.results;
+  return djs.map(function (dj) {
+    var li = createNode('li'),
+        img = createNode('img'),
+        span = createNode('span');
+    img.src = dj.picture.medium;
+    span.innerHTML = ' '+dj.name.first + ' ' + dj.name.last;
+    append(li, img);
+    append(li, span);
+    append(ul, li);
+  });
+})
+
+['catch'](function (error) {
+  console.log(JSON.stringify(error));
+});
